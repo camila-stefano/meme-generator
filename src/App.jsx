931be98cfc229c047793c8c1cc8301ffcfd0215f
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2024-07-02 16:33:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-mono">Meme generator</h1>
+      <div>
+        <p>jlkjjdhhjjsdhjd</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
